ff2e3c26643fc9801ba779c28572c5fbaac48e7b
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-04-30 07:09:53 +0900

    codegenの追加
    openapi-codegenをインストールしてfetcher/* のファイルは自動生成された
    1. `brew instal openapi-codegen`
    2. openapi-codegen.config.ts ファイルの作成
    3. package.json ファイルの編集
    4. `npm install && npm update` の実行
    5. `npm run codegen` でfetcher/* のファイルが自動生成される

---
+++ frontend/openapi-codegen.config.ts
@@ -0,0 +1,24 @@
+import {
+    generateSchemaTypes,
+    generateReactQueryComponents,
+  } from "@openapi-codegen/typescript";
+  import { defineConfig } from "@openapi-codegen/cli";
+  export default defineConfig({
+    isucon: {
+      from: {
+        relativePath: "../openapi/openapi.yaml",
+        source: "file",
+      },
+      outputDir: "./fetcher",
+      to: async (context) => {
+        const filenamePrefix = "isucon";
+        const { schemasFiles } = await generateSchemaTypes(context, {
+          filenamePrefix,
+        });
+        await generateReactQueryComponents(context, {
+          filenamePrefix,
+          schemasFiles,
+        });
+      },
+    },
+  });
