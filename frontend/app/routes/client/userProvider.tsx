8ba9ddc92702606f666eebc71321442c9efd60a0
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-05-15 06:33:59 +0900

    request provider create

--- frontend/app/routes/client/userProvider.tsx
+++ frontend/app/routes/client/userProvider.tsx
@@ -1,5 +1,7 @@
 import { useSearchParams } from "@remix-run/react";
 import { ReactNode, createContext, useContext } from "react";
+import { useAppGetNotification } from "~/apiClient/apiComponents";
+import { AppRequest } from "~/apiClient/apiSchemas";
 
 export type AccessToken = string;
 
@@ -8,26 +10,57 @@ type User = {
   name: string;
   accessToken: AccessToken;
 };
+
 const userContext = createContext<Partial<User>>({});
+const requestContext = createContext<AppRequest | undefined>(undefined);
+
+const RequestProvider = ({
+  children,
+  accessToken,
+}: {
+  children: ReactNode;
+  accessToken: string;
+}) => {
+  const { data, error, isLoading } = useAppGetNotification({
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "text/event-stream",
+    },
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error occurred: {JSON.stringify(error, null, 2)}</div>;
+  if (data === undefined) return <div>notifiaction is undefined</div>;
+
+  /**
+   * TODO: SSE処理
+   */
+  return (
+    <requestContext.Provider value={data}>{children}</requestContext.Provider>
+  );
+};
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [searchParams] = useSearchParams();
-  const accessToken = searchParams.get("user_access_token") ?? undefined;
-  if (accessToken === undefined) {
-    return;
+  const accessToken = searchParams.get("access_token") ?? undefined;
+  const id = searchParams.get("user_id") ?? undefined;
+
+  if (accessToken === undefined || id === undefined) {
+    return <div>must set access_token and user_id</div>;
   }
-  /**
-   * TODO: ログイン情報取得処理
-   */
-  const fetchedValue: User = {
-    id: "fetched-id",
-    name: "fetched-name",
-    accessToken,
-  };
 
   return (
-    <userContext.Provider value={fetchedValue}>{children}</userContext.Provider>
+    <userContext.Provider
+      value={{
+        id,
+        accessToken,
+        name: "ISUCON太郎",
+      }}
+    >
+      <RequestProvider accessToken={accessToken}>{children}</RequestProvider>
+    </userContext.Provider>
   );
 };
 
 export const useUser = () => useContext(userContext);
+export const useRequest = () => useContext(requestContext);
--- frontend/app/routes/driver/driverProvider.tsx
+++ frontend/app/routes/driver/driverProvider.tsx
@@ -1,35 +1,66 @@
 import { useSearchParams } from "@remix-run/react";
 import { ReactNode, createContext, useContext } from "react";
+import { useChairGetNotification } from "~/apiClient/apiComponents";
+import { ChairRequest } from "~/apiClient/apiSchemas";
 
 export type AccessToken = string;
 
-type Driver = {
+type User = {
   id: string;
   name: string;
   accessToken: AccessToken;
 };
-const driverContext = createContext<Partial<Driver>>({});
+
+const driverContext = createContext<Partial<User>>({});
+const requestContext = createContext<ChairRequest | undefined>(undefined);
+
+const RequestProvider = ({
+  children,
+  accessToken,
+}: {
+  children: ReactNode;
+  accessToken: string;
+}) => {
+  const { data, error, isLoading } = useChairGetNotification({
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "text/event-stream",
+    },
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error occurred: {JSON.stringify(error, null, 2)}</div>;
+  if (data === undefined) return <div>notifiaction is undefined</div>;
+
+  /**
+   * TODO: SSE処理
+   */
+  return (
+    <requestContext.Provider value={data}>{children}</requestContext.Provider>
+  );
+};
 
 export const DriverProvider = ({ children }: { children: ReactNode }) => {
   const [searchParams] = useSearchParams();
-  const accessToken = searchParams.get("driver_access_token") ?? undefined;
-  if (accessToken === undefined) {
-    return;
+  const accessToken = searchParams.get("access_token") ?? undefined;
+  const id = searchParams.get("user_id") ?? undefined;
+
+  if (accessToken === undefined || id === undefined) {
+    return <div>must set access_token and user_id</div>;
   }
-  /**
-   * TODO: ログイン情報取得処理
-   */
-  const fetchedValue: Driver = {
-    id: "fetched-id",
-    name: "fetched-name",
-    accessToken,
-  };
 
   return (
-    <driverContext.Provider value={fetchedValue}>
-      {children}
+    <driverContext.Provider
+      value={{
+        id,
+        accessToken,
+        name: "ISUCON太郎",
+      }}
+    >
+      <RequestProvider accessToken={accessToken}>{children}</RequestProvider>
     </driverContext.Provider>
   );
 };
 
 export const useDriver = () => useContext(driverContext);
+export const useRequest = () => useContext(requestContext);
