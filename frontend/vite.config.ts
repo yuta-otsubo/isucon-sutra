faf813eb9568b9a4c1d5ffbc326aebc07ac014a7
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-05-16 06:08:44 +0900

    debugのようにlogined後のページの提供を簡単に

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,46 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig, UserConfig } from "vite";
+import { readFileSync, writeFileSync } from "fs";
+import { defineConfig, type UserConfig, type Plugin } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
- const DEFAULT_HOSTNAME = 'localhost';
- const DEFAULT_PORT = 3000;
+const DEFAULT_HOSTNAME = 'localhost';
+const DEFAULT_PORT = 3000;
+
+const DEFAULT_URL = `http://${DEFAULT_HOSTNAME}:${DEFAULT_PORT}`
+
+const getLoginedSearchParamURL = async(target: 'app' | 'driver') => {
+  const fetched = await fetch(`http://localhost:8080/${target}/register`, {
+    "body": JSON.stringify({
+      "username": "testIsuconUser",
+      "firstname": "isucon",
+      "lastname": "isucon",
+      "date_of_birth": "11111111"
+    }),
+    "method": "POST"
+  })
+  let json;
+  if (fetched.status === 500) {
+    json = JSON.parse(readFileSync(`./${target}LocalLogin`).toString())
+  } else {
+    json = await fetched.json()
+    writeFileSync(`./${target}LocalLogin`, JSON.stringify(json))
+  }
+  const id:string = json["id"]
+  const accessToken:string = json["access_token"]
+  const path = target === 'app' ? 'client' : 'driver'
+  return `${DEFAULT_URL}/${path}?access_token=${accessToken}&user_id=${id}`
+}
+
+ const customConsolePlugin: Plugin = {
+  name: 'custom-test-user-login',
+  configureServer(server) {
+    server.httpServer?.once('listening', async() => {
+  
+    console.log(`logined client page: \x1b[32m  ${await getLoginedSearchParamURL('app')} \x1b[0m`);
+      
+    });
+  }
+};
 
 export const config = {
   plugins: [
@@ -16,6 +53,7 @@ export const config = {
       },
     }),
     tsconfigPaths(),
+    customConsolePlugin
   ],
   server: {
     proxy: {
