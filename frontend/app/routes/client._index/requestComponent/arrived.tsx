b0fb63d2b0f97bd755c58465af542a343deef5b4
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-05-17 10:28:11 +0900

    fix

--- frontend/app/routes/client._index/requestComponent/arrived.tsx
+++ frontend/app/routes/client._index/requestComponent/arrived.tsx
@@ -1,5 +1,5 @@
 import type { RequestComponentProps } from "./type";
 
 export const Arrived: RequestComponentProps<"ARRIVED"> = ({ status }) => {
-    return <div>目的地に到着しました 評価して下さい</div>;
+  return <div>目的地に到着しました 評価して下さい</div>;
 };
