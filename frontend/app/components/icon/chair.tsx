111b5864773dd84822fed29a235564824aff1fc6
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-05-17 13:54:52 +0900

    仮のuser icon, chair iconを作成

---
+++ frontend/app/components/icon/chair.tsx
@@ -0,0 +1,16 @@
+import { IconType } from "./type";
+
+export const ChairIcon: IconType = function (props) {
+  return (
+    <svg
+      fill="currentColor"
+      viewBox="0 0 16 16"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      {/* 仮 */}
+      <path d="M2 2 L14 2 L14 14 L2 14 Z" />
+    </svg>
+  );
+};
