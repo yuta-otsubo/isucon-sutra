f592ee03cead2847c70f87f1bcb45aecf05dad13
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-05-20 05:37:37 +0900

    到着画面

--- frontend/app/routes/client._index/requestComponent/arrived.tsx
+++ frontend/app/routes/client._index/requestComponent/arrived.tsx
@@ -1,3 +1,43 @@
+import { Form, useNavigate } from "@remix-run/react";
+import { useRef } from "react";
+import { Avatar } from "~/components/primitives/avatar/avatar";
+import { Button } from "~/components/primitives/button/button";
+import { Modal } from "~/components/primitives/modal/modal";
+import { Rating } from "~/components/primitives/rating/rating";
+import { Text } from "~/components/primitives/text/text";
+
 export const Arrived = () => {
-  return <div>目的地に到着しました 評価して下さい</div>;
+  const modalRef = useRef<{ close: () => void }>(null);
+
+  const handleCloseModal = () => {
+    if (modalRef.current) {
+      modalRef.current.close();
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const onCloseModal = () => {
+    navigate("/client", { replace: true });
+  };
+
+  return (
+    <div>
+      <Modal ref={modalRef} onClose={onCloseModal}>
+        <Form className="h-full flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center gap-6 mb-14">
+            <Text>目的地に到着しました</Text>
+            <Avatar size="lg" />
+          </div>
+          <div className="flex flex-col items-center gap-6 w-80">
+            <Text>ドライビングは快適でしたか？</Text>
+            <Rating name="rating" />
+            <Button type="submit" onClick={handleCloseModal} className="px-4">
+              評価してドライビングを完了
+            </Button>
+          </div>
+        </Form>
+      </Modal>
+    </div>
+  );
 };
