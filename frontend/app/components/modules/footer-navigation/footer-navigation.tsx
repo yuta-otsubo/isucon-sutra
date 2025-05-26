488f0642e71a7db63313056be1d5399c84277d4f
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-05-27 05:52:24 +0900

    Footerの調整

--- frontend/app/components/modules/footer-navigation/footer-navigation.tsx
+++ frontend/app/components/modules/footer-navigation/footer-navigation.tsx
@@ -12,15 +12,18 @@ export const FooterNavigation: FC<{
   navigationMenus: [NavigationMenuType, NavigationMenuType];
 }> = ({ navigationMenus }) => {
   return (
-    <nav className="sticky bottom-0 z-10 border-t border-secondary-border bg-white">
-      <ul className="grid grid-cols-2">
+    <nav className="sticky bottom-[env(safe-area-inset-bottom)] z-10 border-t border-secondary-border bg-white">
+      <ul className="flex justify-around">
         {navigationMenus.map((menu, index) => (
-          <li key={index} className="flex justify-center">
+          <li
+            key={index}
+            className="flex justify-center border-b-4 has-[.active]:border-black"
+          >
             <NavLink
               to={menu.link}
               end
               className={({ isActive }) =>
-                `flex flex-col items-center justify-center gap-1 px-4 py-1.5 text-xs hover:bg-secondary-hover ${isActive ? "pointer-events-none" : ""}`
+                `flex flex-col items-center justify-center gap-1 px-4 py-1.5 text-xs hover:bg-secondary-hover ${isActive ? "pointer-events-none active" : ""}`
               }
             >
               <menu.icon className="size-[24px] stroke-2" />
