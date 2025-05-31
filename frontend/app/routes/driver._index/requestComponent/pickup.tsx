a2d234e9c01fd96102f8c07ffaa01cb071b13a3a
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-06-01 06:10:48 +0900

    Add API call to /chair/requests/{request_id}/depart

--- frontend/app/routes/driver._index/requestComponent/pickup.tsx
+++ frontend/app/routes/driver._index/requestComponent/pickup.tsx
@@ -1,8 +1,25 @@
+import { useCallback } from "react";
+import { useChairPostRequestDepart } from "~/apiClient/apiComponents";
 import { ChairRequest } from "~/apiClient/apiSchemas";
 import { Button } from "~/components/primitives/button/button";
 import { Text } from "~/components/primitives/text/text";
+import { useDriver } from "~/contexts/driver-context";
 
 export const Pickup = ({ data }: { data?: ChairRequest }) => {
+  const driver = useDriver();
+  const { mutate: postChairRequestDepart } = useChairPostRequestDepart();
+
+  const handleDeparture = useCallback(() => {
+    postChairRequestDepart({
+      headers: {
+        Authorization: `Bearer ${driver.accessToken}`,
+      },
+      pathParams: {
+        requestId: data?.request_id ?? "",
+      },
+    });
+  }, [data, driver, postChairRequestDepart]);
+
   return (
     <>
       <div className="h-full text-center content-center bg-blue-200">Map</div>
@@ -20,7 +37,7 @@ export const Pickup = ({ data }: { data?: ChairRequest }) => {
         )}
         <p>{"from -> to"}</p>
         {data?.status === "DISPATCHED" ? (
-          <Button onClick={() => {}}>出発</Button>
+          <Button onClick={() => handleDeparture()}>出発</Button>
         ) : null}
       </div>
     </>
