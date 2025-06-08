322628eb5cfb3dfb7c13d1adf07e454a3c2b0a1e
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-06-08 10:24:51 +0900

    Remove isReception

--- frontend/app/routes/driver._index/requestComponent/reception.tsx
+++ frontend/app/routes/driver._index/requestComponent/reception.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import {
   useChairPostActivate,
   useChairPostDeactivate,
@@ -18,12 +18,10 @@ export const Reception = ({
   { payload: ClientChairRequest["payload"] }
 >) => {
   const driver = useClientChairRequestContext();
-  const [isReception, setReception] = useState<boolean>(false);
   const { mutate: postChairActivate } = useChairPostActivate();
   const { mutate: postChairDeactivate } = useChairPostDeactivate();
 
   const onClickActivate = useCallback(() => {
-    setReception(true);
     postChairActivate({
       headers: {
         Authorization: `Bearer ${driver.auth?.accessToken}`,
@@ -31,7 +29,6 @@ export const Reception = ({
     });
   }, [driver, postChairActivate]);
   const onClickDeactivate = useCallback(() => {
-    setReception(false);
     postChairDeactivate({
       headers: {
         Authorization: `Bearer ${driver.auth?.accessToken}`,
@@ -48,12 +45,9 @@ export const Reception = ({
         />
       ) : null}
       <div className="h-full text-center content-center bg-blue-200">Map</div>
-      <div className="px-4 py-16 block justify-center border-t">
-        {isReception ? (
-          <Button onClick={() => onClickDeactivate()}>受付終了</Button>
-        ) : (
-          <Button onClick={() => onClickActivate()}>受付開始</Button>
-        )}
+      <div className="px-4 py-16 flex justify-center border-t gap-6">
+        <Button onClick={() => onClickActivate()}>受付開始</Button>
+        <Button onClick={() => onClickDeactivate()}>受付終了</Button>
       </div>
     </>
   );
