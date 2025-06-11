7e9f0d088ba7ee89ed3be7f2e210cfd7e0d7e944
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-06-12 06:20:57 +0900

    fmt

--- frontend/app/routes/driver._index/requestComponent/reception.tsx
+++ frontend/app/routes/driver._index/requestComponent/reception.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 import {
-    useChairPostActivate,
-    useChairPostDeactivate,
+  useChairPostActivate,
+  useChairPostDeactivate,
 } from "~/apiClient/apiComponents";
 
 import { Map } from "~/components/modules/map/map";
