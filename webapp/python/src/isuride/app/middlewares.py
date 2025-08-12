8f5965223ab4cf632fc575e25dcff89fd1a9f7e6
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-08-13 06:01:13 +0900

    python: implement owener_get_sales

--- webapp/python/src/isuride/app/middlewares.py
+++ webapp/python/src/isuride/app/middlewares.py
@@ -1,7 +1,7 @@
 from fastapi import Cookie, HTTPException
 from sqlalchemy import text
 
-from .models import User
+from .models import Owner, User
 from .sql import engine
 
 
@@ -11,8 +11,8 @@ def app_auth_middleware(app_session=Cookie(default=None)) -> User:
 
     with engine.begin() as conn:
         row = conn.execute(
-            text("SELECT * FROM users WHERE access_token = :access_token"),
-            {"access_token": app_session},
+            text("SELECT * FROM users WHERE access_token = :accesss_token"),
+            {"accesss_token": app_session},
         ).fetchone()
 
         if not row:
@@ -20,3 +20,20 @@ def app_auth_middleware(app_session=Cookie(default=None)) -> User:
         user = User(**row._mapping)
 
         return user
+
+
+def owner_auth_middleware(app_session=Cookie(default=None)) -> Owner:
+    if not app_session:
+        raise HTTPException(status_code=401, detail="owner_session cookie is required")
+
+    with engine.begin() as conn:
+        row = conn.execute(
+            text("SELECT * FROM owners WHERE access_token = :access_token"),
+            {"access_token": app_session},
+        ).fetchone()
+
+        if not row:
+            raise HTTPException(status_code=401, detail="invalid access token")
+
+        # TODO: _mapping より良いアトリビュートは無いか
+        return Owner(**row._mapping)
--- webapp/python/src/isuride/app/routers/owners.py
+++ webapp/python/src/isuride/app/routers/owners.py
@@ -5,13 +5,18 @@ https://github.com/isucon/isucon14/blob/main/webapp/go/owner_handlers.go
 TODO: このdocstringを消す
 """
 
+from collections import defaultdict
+from collections.abc import MutableMapping
+from datetime import datetime, timezone
 from typing import Annotated
 
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Depends, Response
 from pydantic import BaseModel, StringConstraints
 from sqlalchemy import text
 from ulid import ULID
 
+from ..middlewares import owner_auth_middleware
+from ..models import Chair, Owner, Ride
 from ..sql import engine
 from ..utils import secure_random_str
 
@@ -64,9 +69,87 @@ def owner_post_owners(
     )
 
 
+class ChairSales(BaseModel):
+    id: str
+    name: str
+    sales: int
+
+
+class ModelSales(BaseModel):
+    mode: str
+    sales: int
+
+
+class OwnerGetSalesResponse(BaseModel):
+    total_sales: int
+    chairs: list[ChairSales]
+    models: list[ModelSales]
+
+
 @router.get("/sales")
-def owner_get_sales():
-    pass
+def owner_get_sales(
+    since: int | None = None,
+    until: int | None = None,
+    owner: Owner = Depends(owner_auth_middleware),
+) -> OwnerGetSalesResponse:
+    # TODO: タイムゾーンの扱いに自信なし
+    if since is None:
+        since_dt = datetime.fromtimestamp(0, tz=timezone.utc)
+    else:
+        since_dt = datetime.fromtimestamp(since / 1000, tz=timezone.utc)
+
+    if until is None:
+        until_dt = datetime(9999, 12, 31, 23, 59, 59, tzinfo=timezone.utc)
+    else:
+        until_dt = datetime.fromtimestamp(until / 1000, tz=timezone.utc)
+
+    with engine.begin() as conn:
+        rows = conn.execute(
+            text("SELECT * FROM chairs WHERE owner_id = :owner_id"),
+            {"owner_id": owner.id},
+        )
+        chairs = [Chair(**r) for r in rows.mappings()]
+
+        res = OwnerGetSalesResponse(total_sales=0, chairs=[], models=[])
+        model_sales_by_model: MutableMapping[str, int] = defaultdict(int)
+        for chair in chairs:
+            rows = conn.execute(
+                text(
+                    "SELECT rides.* FROM rides JOIN ride_statuses ON rides.id = ride_statuses.ride_id WHERE chair_id = :chair_id AND status = 'COMPLETED' AND updated_at BETWEEN :since AND :until"
+                ),
+                # TODO: datetime型で大丈夫なんだっけ？
+                {"chair_id": chair.id, "since": since_dt, "until": until_dt},
+            )
+            rides = [Ride(**r) for r in rows.mappings()]
+
+            chair_sales = sum_sales(rides)
+
+            res.total_sales += chair_sales
+            res.chairs.append(
+                ChairSales(id=chair.id, name=chair.name, sales=chair_sales)
+            )
+            model_sales_by_model[chair.model] += chair_sales
+
+        model_sales = []
+        for model, sales in model_sales_by_model.items():
+            model_sales.append(ModelSales(mode=model, sales=sales))
+
+        res.models = model_sales
+
+        return res
+
+
+def sum_sales(rides: list[Ride]) -> int:
+    sale = 0
+    for ride in rides:
+        sale += calculate_sale(ride)
+    return sale
+
+
+def calculate_sale(ride: Ride) -> int:
+    # TODO: implement
+    # return calculateFare(ride.PickupLatitude, ride.PickupLongitude, ride.DestinationLatitude, ride.DestinationLongitude)
+    return 10
 
 
 @router.get("/chairs", status_code=200)
