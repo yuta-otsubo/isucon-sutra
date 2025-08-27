592b220fed6eadb833283b65923fc9d8ab0cf263
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-08-28 05:18:19 +0900

    python: fix payment_geteway.py

--- webapp/python/src/isuride/app/payment_gateway.py
+++ webapp/python/src/isuride/app/payment_gateway.py
@@ -29,45 +29,51 @@ def request_payment_gateway_post_payment(
     # FIXME: 社内決済マイクロサービスのインフラに異常が発生していて、同時にたくさんリクエストすると変なことになる可能性あり
     retry = 0
     while True:
-        res = urllib3.request(
-            "POST",
-            payment_gateway_url + "/payments",
-            json=param.model_dump(),
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {token}",
-            },
-        )
-
-        if res.status != HTTPStatus.NO_CONTENT:
-            # エラーが返ってきても成功している場合があるので、社内決済マイクロサービスに問い合わせ
-            get_res = urllib3.request(
-                "GET",
+        try:
+            res = urllib3.request(
+                "POST",
                 payment_gateway_url + "/payments",
+                json=param.model_dump(),
                 headers={
+                    "Content-Type": "application/json",
                     "Authorization": f"Bearer {token}",
                 },
+                retries=False,
             )
 
-            # GET /payments は障害と関係なく200が返るので、200以外は回復不能なエラーとする
-            if get_res.status != HTTPStatus.OK:
-                raise RuntimeError(
-                    f"[GET /payments] unexpected status code ({get_res.status})"
-                )
-            payments = [
-                PaymentGatewayGetPaymentsResponseOne(**item) for item in get_res.json()
-            ]
-
-            rides = retrieve_rides_order_by_created_at_asc()
-
-            if len(rides) != len(payments):
-                raise RuntimeError(
-                    f"unexpected number of payments: {len(rides)}  != {len(payments)}. {ERRORED_UPSTREAM}",
+            if res.status != HTTPStatus.NO_CONTENT:
+                # エラーが返ってきても成功している場合があるので、社内決済マイクロサービスに問い合わせ
+                get_res = urllib3.request(
+                    "GET",
+                    payment_gateway_url + "/payments",
+                    headers={
+                        "Authorization": f"Bearer {token}",
+                    },
+                    retries=False,
                 )
 
-        if retry < 5:
-            retry += 1
-            time.sleep(0.1)
-            continue
+                # GET /payments は障害と関係なく200が返るので、200以外は回復不能なエラーとする
+                if get_res.status != HTTPStatus.OK:
+                    raise RuntimeError(
+                        f"[GET /payments] unexpected status code ({get_res.status})"
+                    )
+                payments = [
+                    PaymentGatewayGetPaymentsResponseOne(**item)
+                    for item in get_res.json()
+                ]
 
-        break
+                rides = retrieve_rides_order_by_created_at_asc()
+
+                if len(rides) != len(payments):
+                    raise RuntimeError(
+                        f"unexpected number of payments: {len(rides)}  != {len(payments)}. {ERRORED_UPSTREAM}",
+                    )
+        except Exception:
+            if retry < 5:
+                retry += 1
+                time.sleep(0.1)
+                continue
+            else:
+                raise
+        else:
+            break
