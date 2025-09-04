c643f53dd13a01a72b195ea89368d68f2a03f0f2
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-09-05 05:08:48 +0900

    python: improve millisec timestamp

--- webapp/python/src/isuride/app/sql.py
+++ webapp/python/src/isuride/app/sql.py
@@ -1,4 +1,13 @@
+import os
+
 import sqlalchemy
 
-# TODO: 環境変数からホスト名などを読むようにする
-engine = sqlalchemy.create_engine("mysql+pymysql://isucon:isucon@localhost/isuride")
+host = os.getenv("ISUCON_DB_HOST", "127.0.0.1")
+port = int(os.getenv("ISUCON_DB_PORT", "3306"))
+user = os.getenv("ISUCON_DB_USER", "isucon")
+password = os.getenv("ISUCON_DB_PASSWORD", "isucon")
+dbname = os.getenv("ISUCON_DB_NAME", "isuride")
+
+engine = sqlalchemy.create_engine(
+    f"mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}"
+)
