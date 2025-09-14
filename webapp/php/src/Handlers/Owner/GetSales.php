5376aa49ce5730ea566b27cb59792ef1ad2ef085
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-09-15 05:17:06 +0900

    [PHP]sales集計クエリの日付形式を修正

--- webapp/php/src/Handlers/Owner/GetSales.php
+++ webapp/php/src/Handlers/Owner/GetSales.php
@@ -117,8 +117,8 @@ class GetSales extends AbstractHttpHandler
                 WHERE chair_id = ? AND status = \'COMPLETED\' AND updated_at BETWEEN ? AND ?  + INTERVAL 999 MICROSECOND'
                 );
                 $stmt->bindValue(1, $chair->id, PDO::PARAM_STR);
-                $stmt->bindValue(2, $since->format('Y-m-d H:i:s'), PDO::PARAM_STR);
-                $stmt->bindValue(3, $until->format('Y-m-d H:i:s'), PDO::PARAM_STR);
+                $stmt->bindValue(2, $since->format('Y-m-d H:i:s.v'), PDO::PARAM_STR);
+                $stmt->bindValue(3, $until->format('Y-m-d H:i:s.v'), PDO::PARAM_STR);
                 $stmt->execute();
                 $rides = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 $sumChairSales = $this->sumSales($rides);
