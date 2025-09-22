d4e775689624ac718af99b250261c571b54fd883
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-09-23 05:09:11 +0900

    python: fix middlewares and main

--- webapp/python/src/isuride/app/middlewares.py
+++ webapp/python/src/isuride/app/middlewares.py
@@ -5,16 +5,21 @@ https://github.com/isucon/isucon14/blob/main/webapp/go/middlewares.go
 TODO: このdocstringを消す
 """
 
-from fastapi import Cookie, HTTPException, status
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import Cookie, HTTPException
 from sqlalchemy import text
 
 from .models import Chair, Owner, User
 from .sql import engine
 
 
-def app_auth_middleware(app_session=Cookie(default=None)) -> User:
+def app_auth_middleware(app_session: Annotated[str | None, Cookie()] = None) -> User:
     if not app_session:
-        raise HTTPException(status_code=401, detail="app_session cookie is required")
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED, detail="app_session cookie is required"
+        )
 
     with engine.begin() as conn:
         row = conn.execute(
@@ -23,15 +28,22 @@ def app_auth_middleware(app_session=Cookie(default=None)) -> User:
         ).fetchone()
 
         if not row:
-            raise HTTPException(status_code=401, detail="invalid access token")
+            raise HTTPException(
+                status_code=HTTPStatus.UNAUTHORIZED, detail="invalid access token"
+            )
         user = User(**row._mapping)
 
         return user
 
 
-def owner_auth_middleware(owner_session=Cookie(default=None)) -> Owner:
+def owner_auth_middleware(
+    owner_session: Annotated[str | None, Cookie()] = None,
+) -> Owner:
     if not owner_session:
-        raise HTTPException(status_code=401, detail="owner_session cookie is required")
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="owner_session cookie is required",
+        )
 
     with engine.begin() as conn:
         row = conn.execute(
@@ -40,15 +52,22 @@ def owner_auth_middleware(owner_session=Cookie(default=None)) -> Owner:
         ).fetchone()
 
         if not row:
-            raise HTTPException(status_code=401, detail="invalid access token")
+            raise HTTPException(
+                status_code=HTTPStatus.UNAUTHORIZED, detail="invalid access token"
+            )
 
         # TODO: _mapping より良いアトリビュートは無いか
         return Owner(**row._mapping)
 
 
-def chair_auth_middleware(chair_session=Cookie(default=None)) -> Chair:
+def chair_auth_middleware(
+    chair_session: Annotated[str | None, Cookie()] = None,
+) -> Chair:
     if not chair_session:
-        raise HTTPException(status_code=401, detail="chair_session cookie is required")
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="chair_session cookie is required",
+        )
 
     with engine.begin() as conn:
         row = conn.execute(
@@ -58,7 +77,7 @@ def chair_auth_middleware(chair_session=Cookie(default=None)) -> Chair:
 
         if not row:
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid access token"
+                status_code=HTTPStatus.UNAUTHORIZED, detail="invalid access token"
             )
 
         # TODO: _mapping より良いアトリビュートは無いか
