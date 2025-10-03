fdaa10d86834346009057768e33aed8905115455
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-10-04 05:05:19 +0900

    [fe]owner登録のバリデーション実装

--- frontend/app/types.ts
+++ frontend/app/types.ts
@@ -55,3 +55,35 @@ export type Pos = {
 };
 
 export type Coordinate = ApiCoodinate;
+
+export type ClientApiError = {
+  message: string;
+  name: string;
+  stack: {
+    payload: string;
+    status: number;
+  };
+};
+
+export function isClientApiError(e: unknown): e is ClientApiError {
+  if (typeof e === "object" && e !== null) {
+    const typedError = e as {
+      name?: unknown;
+      message?: unknown;
+      stack?: {
+        status?: unknown;
+        payload?: unknown;
+      };
+    };
+
+    return (
+      typeof typedError.name === "string" &&
+      typeof typedError.message === "string" &&
+      typeof typedError.stack === "object" &&
+      typedError.stack !== null &&
+      typeof typedError.stack.status === "number" &&
+      typeof typedError.stack.payload === "string"
+    );
+  }
+  return false;
+}
