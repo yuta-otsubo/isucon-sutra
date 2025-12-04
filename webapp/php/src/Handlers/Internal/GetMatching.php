40409fe57bcc1409d3bde455a635c17ef4cda0f5
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-12-05 05:09:01 +0900

    php: ride status判定修正

--- webapp/php/src/Handlers/Internal/GetMatching.php
+++ webapp/php/src/Handlers/Internal/GetMatching.php
@@ -12,7 +12,6 @@ use PDO;
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 class GetMatching extends AbstractHttpHandler
 {
-
     public function __construct(
         private readonly PDO $db,
     ) {
@@ -30,7 +29,6 @@ class GetMatching extends AbstractHttpHandler
         if (!$ride) {
             return $this->writeNoContent($response);
         }
-
         for ($i = 0; $i < 10; $i++) {
             $stmt = $this->db->prepare(
                 'SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1'
@@ -44,17 +42,15 @@ class GetMatching extends AbstractHttpHandler
                 'SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE'
             );
             $stmt->execute([$matched['id']]);
-            $empty = $stmt->fetch(PDO::FETCH_ASSOC);
-
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $empty = $result['COUNT(*) = 0'];
             if ($empty) {
                 break;
             }
         }
-
         if (!$empty) {
             return $this->writeNoContent($response);
         }
-
         $stmt = $this->db->prepare('UPDATE rides SET chair_id = ? WHERE id = ?');
         $stmt->execute([$matched['id'], $ride['id']]);
         return $this->writeNoContent($response);
