93ef27f7a98b38400316ab98389af7155fd18ce3
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-12-24 05:09:17 +0900

    [FE]評価後の現在地、目的地をリセットするように

--- frontend/app/routes/client._index/route.tsx
+++ frontend/app/routes/client._index/route.tsx
@@ -272,6 +272,8 @@ export default function Index() {
             <Arrived
               onEvaluated={() => {
                 statusModalRef.current?.close();
+                setCurrentLocation(destLocation);
+                setDestLocation(undefined);
               }}
             />
           )}
